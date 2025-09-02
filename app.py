b1b47f7cd9f1f82161e4d9b1d2ca966e3ecd3e78
BastekGarage <kadett126@gmail.com>
2025-09-02 18:08:16 +0200

    app.py

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ CONSTANT_FORMATKI = 13140.56
 st.title("Kalkulator Gramatur")
 
 # Karty
-tab1, tab2 = st.tabs(["Siatki", "Formatki"])
+tab1, tab2 = st.tabs(["Owata", "Formatki"])
 
 # Karta Siatki
 with tab1:
-    st.header("Kalkulator dla owaty")
+    st.header("Kalkulator dla Owaty")
     speed_siatki = st.number_input("Prędkość maszyny (%) (Siatki)", min_value=0.0, value=64.0, step=0.1, key="speed_siatki")
     stretch = st.number_input("Rozciąg siatek (%)", min_value=0.0, value=162.0, step=0.1, key="stretch")
     grammage_siatki = st.number_input("Gramatura (g/m²) (Siatki)", min_value=0.0, value=120.0, step=0.1, key="grammage_siatki")
@@ -29,7 +29,7 @@ with tab1:
 
     layers_siatki = st.number_input("Ilość ułożeń (Siatki)", min_value=0.0, value=default_layers_siatki, step=0.1, key="layers_siatki")
 
-    if st.button("Oblicz (Siatki)", key="calculate_siatki"):
+    if st.button("Oblicz (Owata)", key="calculate_siatki"):
         if speed_siatki <= 0 or stretch <= 0 or grammage_siatki <= 0 or layers_siatki <= 0:
             st.error("Wpisz poprawne dane (większe od 0)!")
         elif speed_siatki > 100:
